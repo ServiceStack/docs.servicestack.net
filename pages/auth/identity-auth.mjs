d36f1d1b9583f5d17b464f16c2bfc54744cfe508
GitHub Action <action@github.com>
2024-06-15 02:33:59 +0000

    deploy: bdb243518d82458acc2aab8df6ee40ab0f20f38e

--- pages/auth/identity-auth.mjs
+++ pages/auth/identity-auth.mjs
@@ -2,7 +2,7 @@ import Templates, { Index } from "../templates/Templates.mjs"
 
 const IdentityAuthTemplates = {
     components: { Templates },
-    template:`<Templates :templates="[Index['blazor'], Index['blazor-vue'], Index['razor'], Index['mvc'], Index['razor-bootstrap'], Index['mvc-bootstrap']]" hide="demo" />`,
+    template:`<Templates :templates="[Index['blazor'], Index['blazor-vue'], Index['blazor-wasm'], Index['razor'], Index['mvc'], Index['razor-bootstrap']]" hide="demo" />`,
     setup() {
         return { Index }
     }
