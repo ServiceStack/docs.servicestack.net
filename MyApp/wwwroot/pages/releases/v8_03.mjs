c387bc229da23486636b305e7873dafd020a83b4
Demis Bellot <demis.bellot@gmail.com>
2024-09-23 17:50:37 +0800

    Add podcast episode AudioPlayer on all v8.x release notes

--- MyApp/wwwroot/pages/releases/v8_03.mjs
+++ MyApp/wwwroot/pages/releases/v8_03.mjs
@@ -1,23 +1,7 @@
 import Templates, { Index } from "../templates/Templates.mjs"
 import AudioPlayer from "../podcasts/AudioPlayer.mjs"
 
-const BlazorTemplate = {
-    components: { Templates },
-    template:`<Templates :templates="[Index['blazor']]" />`,
-    setup() {
-        return { Index }
-    }
-}
-
-const BlazorVueTemplate = {
-    components: { Templates },
-    template:`<Templates :templates="[Index['blazor-vue']]" />`,
-    setup() {
-        return { Index }
-    }
-}
-
-const IdentityAuthTemplates = {
+const AuthTemplates = {
     components: { Templates },
     template:`<Templates :templates="[Index['blazor'], Index['blazor-vue'], Index['razor'], Index['mvc'], Index['razor-bootstrap'], Index['mvc-bootstrap']]" hide="demo" />`,
     setup() {
@@ -30,9 +14,7 @@ export default {
     },
     components: {
         AudioPlayer,
-        BlazorTemplate,
-        BlazorVueTemplate,
-        IdentityAuthTemplates,
+        AuthTemplates,
     },
     setup() {
         return { }
