0ce496702baca06784f16e6deaa48ef9e5e20998
GitHub Action <action@github.com>
2024-11-26 14:35:21 +0000

    deploy: 2b3975189416637982a64c2ed093cbdbd13bb429

--- pages/releases/v8_05.mjs
+++ pages/releases/v8_05.mjs
@@ -1,9 +1,11 @@
+import AudioPlayer from "../podcasts/AudioPlayer.mjs"
 import AsciiCinema from '../components/AsciiCinema.mjs'
 
 export default {
     install(app) {
     },
     components: {
+        AudioPlayer,
         AsciiCinema,
     },
     setup() {
