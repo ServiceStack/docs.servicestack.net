80a74a3eae0e6d217408913b476cfcba7fdd0c4d
GitHub Action <action@github.com>
2025-11-13 16:13:43 +0000

    deploy: ee5c10d09ae7cceb3bb99e9e6fdef7d8bbf4802d

---
+++ pages/react/overview.mjs
@@ -0,0 +1,14 @@
+import ReactTemplate from "../components/ReactTemplate.mjs"
+import ScreenshotsGallery from "../components/ScreenshotsGallery.mjs"
+
+export default {
+    install(app) {
+    },
+    components: {
+        ReactTemplate,
+        ScreenshotsGallery,
+    },
+    setup() {
+        return { }
+    }
+}
